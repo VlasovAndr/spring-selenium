d4e0c3c770819ae32f0111e40be635c742c34f3e
Andrey Vlasov <lion32211@yandex.ru>
2025-02-23 15:29:37 +0100

    Added parallel test execution func

--- src/test/java/com/udemy/spring/spring_selenium/CucumberRunner.java
+++ src/test/java/com/udemy/spring/spring_selenium/CucumberRunner.java
@@ -2,15 +2,24 @@ package com.udemy.spring.spring_selenium;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
+        strict = true,
         features = "classpath:features",
         glue = "com.udemy.spring.spring_selenium.bdd",
+        tags = {"@smoke"},
         plugin = {
                 "pretty",
                 "html:D:\\JavaProjects\\Output"
         }
-
 )
+
 public class CucumberRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
--- src/test/java/com/udemy/spring/spring_selenium/bdd/CucumberHooks.java
+++ src/test/java/com/udemy/spring/spring_selenium/bdd/CucumberHooks.java
@@ -1,19 +1,30 @@
 package com.udemy.spring.spring_selenium.bdd;
 
+import com.udemy.spring.spring_selenium.autoframework.annotation.LazyAutowired;
 import com.udemy.spring.spring_selenium.autoframework.service.ScreenShotService;
+import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openqa.selenium.WebDriver;
+import org.springframework.context.ApplicationContext;
 
 public class CucumberHooks {
 
-    @Autowired
+    @LazyAutowired
     private ScreenShotService screenShotService;
+
+    @LazyAutowired
+    protected ApplicationContext ctx;
 
     @AfterStep
     public void afterStep(Scenario scenario) {
         if (scenario.isFailed()) {
-            scenario.embed(screenShotService.getScreenShot(), "image/png", scenario.getName());
+            scenario.embed(this.screenShotService.getScreenShot(), "image/png", scenario.getName());
         }
+    }
+
+    @After
+    public void afterScenario() {
+        this.ctx.getBean(WebDriver.class).quit();
     }
 }
